d8fc6b9e4abf9231842e252d8fbce3afe62c25ea
level237 <bramslevel129@gmail.com>
2025-10-28 23:26:03 +0100

    update

--- roomify/settings.py
+++ roomify/settings.py
@@ -121,3 +121,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "core/static"
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
+
+MEDIA_ROOT = BASE_DIR / "media"
