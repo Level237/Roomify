9a5db3ed186ecef94a77633dae2b43c0166fb765
level237 <bramslevel129@gmail.com>
2025-10-29 15:08:27 +0100

    update

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,15 +1,20 @@
 from  django import forms
 from django.contrib.auth.models import User
-
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, get_user_model
 
 class CustomRegisterForm(forms.ModelForm):
     
-    password = forms.CharField(widget=forms.PasswordInput)
-    confirm_password= forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control','placeholder':'Enter your password'}))
+    confirm_password= forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control','placeholder':'Enter your confirm password'}))
     
     class Meta:
         model= User
-        fields= ['username',"email","password"],
+        fields= ['username',"email","password"]
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter a username'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Enter your email'}),
+        }
         
     def clean(self):
         cleaned_data=super().clean()
@@ -19,3 +24,40 @@ class CustomRegisterForm(forms.ModelForm):
         if password and confirm and password != confirm:
             self.add_error("confirm_password", " Password doesn't not match")
         return cleaned_data
+    
+
+class CustomLoginForm(AuthenticationForm):
+    
+    email=forms.EmailField(
+        label="Email address",
+        widget=forms.TextInput(attrs={
+            'class': "form-control",
+            'placeholder': 'Enter a email address'
+        })
+    )
+    
+    password = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(attrs={
+            'class':"form-control",
+            'placeholder':"Enter a password"
+        })
+    )
+    
+    def clean(self):
+        email=self.cleaned_data.get("email")
+        password=self.cleaned_data.get("password")
+        
+        if email and password:
+            try:
+                user = User.objects.get(email=email)
+            except User.DoesNotExist:
+                raise forms.ValidationError("User does not exist")
+            
+            user= authenticate(username=user.email,password=password)
+            if user is None:
+                raise forms.ValidationError("Invalid password")
+            self.user_cache = user
+        return self.cleaned_data
+    
+    
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,29 @@
+from .forms import CustomRegisterForm,CustomLoginForm
 from django.shortcuts import render,redirect
+from django.contrib.auth import login
 
+def register_user(request):
+    if request.method == "POST":
+        form= CustomRegisterForm(request.POST)
+        if form.is_valid():
+            user=form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            login(request,user)
+            return redirect('core:home')
+    else:
+        form=CustomRegisterForm()
+    return render(request,'accounts/register.html', {'form': form})
 
-
+def login_user(request):
+    if request.method == "POST":
+        form=CustomLoginForm(request,data=request.POST)
+        if form.is_valid():
+            user=form.get_user()
+            login(request,user)
+            return redirect('core:home')
+    else:
+        form =CustomLoginForm()
+    return render(request,'accounts/login.html',{'form':form})
+            
+        
