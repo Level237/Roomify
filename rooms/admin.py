d34106067f56262c64d01cfc748bfffc4ec4d313
level237 <bramslevel129@gmail.com>
2025-10-31 22:18:41 +0100

    update

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
-from .models import Room
+from .models import Room, RoomImage
 
 # Register your models here.
 
+class RoomImageInline(admin.TabularInline):
+    model = RoomImage
+    extra = 1
+    fields = ('image',)
+    verbose_name = " Room Image"
+    verbose_name_plural = "Galery of room"
+    
+
 class RoomAdmin(admin.ModelAdmin):
     list_display = ('hotel','room_number','room_type','price_per_night','is_available','capacity','room_profile')
-    
+    inlines = [RoomImageInline]
 admin.site.register(Room,RoomAdmin)
