33e23a9bb399c496243eee452f6d982a57935e37
level237 <bramslevel129@gmail.com>
2025-10-31 02:12:34 +0100

    signal assign_super_user role

---
+++ accounts/signals.py
@@ -0,0 +1,14 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth import get_user_model
+from roles.models import Role
+
+user = get_user_model()
+
+@receiver(post_save,sender=user)
+
+def assign_admin_role(sender,instance,created,**kwargs):
+    if created and instance.is_superuser:
+        role,_ = Role.objects.get_or_create(name="admin")
+        instance.role=role
+        instance.save()
